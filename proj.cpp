92b2e9c93213777f5e30ec91921a10ddb8c1a509
msaaddev <mrsaadirfan@gmail.com>
2019-06-02 23:18:21 +0500

    📦NEW: default constructor

--- proj.cpp
+++ proj.cpp
@@ -3,6 +3,24 @@
 #include <cstring>
 using namespace std;
 
+class playList
+{
+protected:
+
+    int size;
+    Song *songs = new songs[size];
+
+public:
+    playList();
+
+};
+
+playList::playList()
+{
+    size = 2;
+}
+
+
 int main()
 {
 }
