2e8d322b101950d5395ad7cb4a34745398341010
msaaddev <mrsaadirfan@gmail.com>
2019-06-03 02:34:29 +0500

    📦NEW: function structures

--- proj.cpp
+++ proj.cpp
@@ -8,23 +8,37 @@ class playList
 protected:
 
     int size;
-    Song *songs = new songs[size];
+    Song *songs = new Song[size];
 
 public:
-    playList();
-    void AddSong(const Song &s);
-    bool DeleteSong(const Song &s);
-    void ShowAll() const;
-    void play(int num=1);
-    void ShowStatus() const;
 
+    playList ();
+    void AddSong ( const Song &s );
+    bool DeleteSong ( const Song &s );
+    void ShowAll ( ) const;
+    void play ( int num = 1 );
+    void ShowStatus ( ) const;
 };
 
-playList::playList()
+playList :: playList( )
 {
     size = 2;
 }
 
+void playList :: AddSong ( songs )
+{
+
+}
+
+bool playList :: DeleteSong ( songs )
+{
+
+}
+
+void playList :: ShowAll ( ) const
+{
+
+}
 
 int main()
 {
