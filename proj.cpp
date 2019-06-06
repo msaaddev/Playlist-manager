4f58596c97b9e2a548bbf689a9cea498935dc26a
msaaddev <mrsaadirfan@gmail.com>
2019-06-06 20:42:25 +0500

    📦NEW: deleteSong definition

--- proj.cpp
+++ proj.cpp
@@ -6,64 +6,72 @@ using namespace std;
 class playList
 {
 private:
-
     int size, noOfSongs, currentSongIndex, index;
     Song *PlayList = new Song[size];
 
 public:
-
-    playList ();
-    void AddSong ( const Song &s );
-    bool DeleteSong ( const Song &s );
-    void ShowAll ( ) const;
-    void play ( int num = 1 );
-    void ShowStatus ( ) const;
-    void doublesize ( );
+    playList();
+    void AddSong(const Song &s);
+    bool DeleteSong(const Song &s);
+    void ShowAll() const;
+    void play(int num = 1);
+    void ShowStatus() const;
+    void doublesize();
 };
 
-playList :: playList( )
+playList ::playList()
 {
     size = 2;
 }
 
-void playList :: AddSong ( const Song &s )
+void playList ::AddSong(const Song &s)
 {
     PlayList[index] = s;
     currentSongIndex = index;
     index++;
 }
 
-bool playList :: DeleteSong ( const Song &s )
+bool playList ::DeleteSong(const Song &s)
 {
-
+    bool check = false;
+    for (int i = 0; i < index; i++)
+    {
+        if (PlayList[i] == s)
+        {
+            for (i; i < index; i++)
+            {
+                PlayList[i] = PlayList[i + 1];
+            }
+            check = true;
+        }
+    }
+    return check;
 }
 
-void playList :: ShowAll ( ) const
+void playList ::ShowAll() const
 {
-
 }
 
-void playList :: play ( int num )
+void playList ::play(int num)
 {
-
 }
 
-void playList::doublesize ()
+void playList::doublesize()
 {
     if (size == noOfSongs)
     {
-        size = size*=2;
+        size = size *= 2;
     }
-    else if (noOfSongs < (size/2))
+    else if (noOfSongs < (size / 2))
     {
-        if(size != 2)
+        if (size != 2)
         {
-            size/=2;
+            size /= 2;
         }
     }
 }
 
-void playList:: ShowStatus ( ) const
+void playList::ShowStatus() const
 {
     cout << "Size of the song array: " << size << endl;
     cout << "No of Songs in the array: " << noOfSongs << endl;
